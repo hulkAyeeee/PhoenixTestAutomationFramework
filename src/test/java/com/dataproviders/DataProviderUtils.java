99a20eb267afb5ed5b42b69bcd2076c6898a2f08
Nikhil <nik7hulke@gmail.com>
2026-04-02 15:53:44 +0530

    Created FakeDataGeneratorUtil and also added CreateJobAPIFakeDataDrivenTest into datadriven package

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -9,6 +9,7 @@ import org.testng.annotations.DataProvider;
 import com.api.request.model.CreateJobPayload;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
+import com.api.utils.FakerDataGenerator;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -40,6 +41,11 @@ public class DataProviderUtils {
 		}
 		
 		return payloadList.iterator();
+	}
+	
+	@DataProvider(name="CreateJobAPIFakeDataProvider", parallel = true )
+	public static Iterator<CreateJobPayload> createJobFakeDataProvider() {
+		 return FakerDataGenerator.generateFakeCreateJobData(10);
 	}
 	
 }
